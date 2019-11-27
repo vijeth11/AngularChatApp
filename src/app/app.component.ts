954a5ddbf4a1c95370cdc8da8e5890cce6d37252
Vijeth Ashok Belle <vbelle@BLR.ALLEGISINDIA.COM>
2019-11-27 08:21:01 -0500

    updated some changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import {MatFormField,} from '@angular/material/form-field';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit,AfterViewInit{
+export class AppComponent {
 
   @ViewChild('userInfoScreen',{static:true}) userInfoScreen: ElementRef;
   @ViewChild('completeScreen',{static:true}) completeScreen:ElementRef;
@@ -15,18 +15,9 @@ export class AppComponent implements OnInit,AfterViewInit{
   title = 'angularChat';
   AddContactButtonColor = "#32465A";
   AddNewContactButtonColor = "#32465A";
-  HeightOfTheUserList = "85.5vh"
-
-  ngOnInit(){
-    this.HeightOfTheUserList = String(this.completeScreen.nativeElement.offsetHeight - this.userInfoScreen.nativeElement.offsetHeight-this.footerScreen.nativeElement.offsetHeight)+"px";
-  }
-
-  ngAfterViewInit(): void {
-    this.HeightOfTheUserList = "77.5%";
-  }
-
+  HeightOfTheUserList = "85.5%"
   expanded(data:boolean){
-    this.HeightOfTheUserList = String(this.completeScreen.nativeElement.offsetHeight - this.userInfoScreen.nativeElement.offsetHeight-this.footerScreen.nativeElement.offsetHeight)+"px";
+    //this.HeightOfTheUserList = String(this.completeScreen.nativeElement.offsetHeight - this.userInfoScreen.nativeElement.offsetHeight-this.footerScreen.nativeElement.offsetHeight)+"px";
     console.log(this.userInfoScreen.nativeElement.offsetHeight);
   }
 }
