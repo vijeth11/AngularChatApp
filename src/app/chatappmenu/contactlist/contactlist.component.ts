111adb687c1c7110b84030ce442b42c060a4bba5
vijeth <vijeth11@yahoo.co.in>
2019-12-06 06:30:32 +1200

    added search filter for contactlist menu

--- src/app/chatappmenu/contactlist/contactlist.component.ts
+++ src/app/chatappmenu/contactlist/contactlist.component.ts
@@ -7,12 +7,17 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ContactlistComponent implements OnInit {
 
-  @Input() messages:{from:string,display:string,color:string,setDefault:boolean}[];
+  @Input('messages') messagesInput:{from:string,display:string,color:string,setDefault:boolean}[];
   @Input()HeightOfTheUserList:string;
   @Input()maxHeightOfUserList:string;
-  constructor() { }
+  messages:{from:string,display:string,color:string,setDefault:boolean}[];
+  searchName:string="";
+  constructor() { 
+    
+  }
 
   ngOnInit() {
+      this.messages = this.messagesInput;
   }
 
   setBackgroundColor(index:any,type:string){
@@ -24,5 +29,15 @@ export class ContactlistComponent implements OnInit {
   setdefaultcolor(index:any){
        this.messages[index].setDefault=!this.messages[index].setDefault;
        this.messages[index].color = this.messages[index].setDefault?"#32465A":"#2C3E50";
+  }
+
+  onkeyUp(event:any){
+    this.searchName = event.target.value;
+    console.log(event);
+    if(this.searchName){
+      this.messages = this.messagesInput.filter(x => x.from.toLowerCase().includes(this.searchName.toLowerCase()));
+    }else{
+      this.messages = this.messagesInput;
+    }
   }
 }
